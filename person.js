ebbf7a17fb08dc44d2ca18419f641de75b5909d3
Admin <vanpho02@gmail.com>
2017-02-18 14:12:47 +0700

    stringify

--- person.js
+++ person.js
@@ -3,29 +3,29 @@
   age
 */
 
-// function Person (name, age) {
-//   this.name = name;
-//   this.age = age;
-//   this.addAge = () => {
-//     this.age++;
-//   }
-//   this.chao = () => {
-//     console.log('Chao minh la: ' + this.name + ', ' + this.age + ' tuoi');
-//   }
-// }
-
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  addAge(){
+function Person (name, age) {
+  this.name = name;
+  this.age = age;
+  this.addAge = () => {
     this.age++;
   }
-  chao(){
+  this.chao = () => {
     console.log('Chao minh la: ' + this.name + ', ' + this.age + ' tuoi');
   }
 }
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   addAge(){
+//     this.age++;
+//   }
+//   chao(){
+//     console.log('Chao minh la: ' + this.name + ', ' + this.age + ' tuoi');
+//   }
+// }
 
 let p1 = new Person('Pho', 18);
 let p2 = new Person('Huong', 30);
@@ -53,4 +53,6 @@ Person.prototype.showInfo = function () {
 let p3 = Object.assign({}, p1);
 p1.name = 'PPP';
 console.log(p3);
-console.log(p1);
+// console.log(p1);
+
+console.log(JSON.stringify(p1));
