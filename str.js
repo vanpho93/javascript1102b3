3eced992d4808864569b750753a34766b3542fb6
Admin <vanpho02@gmail.com>
2017-02-18 15:21:24 +0700

    split

---
+++ str.js
@@ -0,0 +1,8 @@
+let str1 = `"abc''"`;
+console.log(str1);
+
+let name = 'Pho';
+
+let hello = name + ' xin chao ban';
+let hi = `${name} xin chao ban`;
+console.log(hi);
---
+++ str2.js
@@ -0,0 +1,12 @@
+let khoahoc = "Android, iOS, Node";
+
+// console.log(khoahoc.lastIndexOf('z'));
+// console.log(khoahoc.charAt(2));
+// console.log(khoahoc.substring(9, 12));
+//
+// console.log(khoahoc.concat(', PHP'));
+// console.log(khoahoc.toUpperCase());
+
+// console.log(khoahoc.split(', '));
+
+console.log(khoahoc.replace(', ', ','));
