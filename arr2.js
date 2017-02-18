11c1b94db99eb2966aa24510ad695cffe5b1e0bc
Admin <vanpho02@gmail.com>
2017-02-18 15:03:21 +0700

    array

--- arr2.js
+++ arr2.js
@@ -1,10 +1,17 @@
 let mang = [1, 3, 2, 17, 3];
-let arr = mang.sort();
-console.log(arr);
+// let arr = mang.sort();
+// console.log(arr);
+//
+// let arr2 = mang.sort(function(a, b){
+//   if(a > b) return 1;
+//   if(a < b) return -1;
+//   if(a == b) return 0;
+// });
+// console.log(arr2);
 
-let arr2 = mang.sort(function(a, b){
-  if(a > b) return 1;
-  if(a < b) return -1;
-  if(a == b) return 0;
+let arr = mang.map(function(e, i){
+  return e * 2;
 });
-console.log(arr2);
+
+
+console.log(arr);
